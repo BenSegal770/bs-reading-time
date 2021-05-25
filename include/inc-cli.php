1673a9f1881eec031458777880a968aeaf849579
BenSegal770 <bensegal2@gmail.com>
2021-05-25 22:17:01 +0300

    first commit

---
+++ include/inc-cli.php
@@ -0,0 +1,40 @@
+<?php
+
+class Example_Command {
+
+    /**
+     * Prints a greeting.
+     *
+     * ## OPTIONS
+     *
+     * <name>
+     * : The name of the person to greet.
+     *
+     * [--type=<type>]
+     * : Whether or not to greet the person with success or error.
+     * ---
+     * default: success
+     * options:
+     *   - success
+     *   - error
+     * ---
+     *
+     * ## EXAMPLES
+     *
+     *     wp example hello Newman
+     *
+     * @when after_wp_load
+     */
+     
+    function hello( $args, $assoc_args ) {
+        list( $name ) = $args;
+
+        // Print the message with type
+        $type = $assoc_args['type'];
+        WP_CLI::$type( "Hello, $name!" );
+    }
+}
+
+WP_CLI::add_command( 'example', 'Example_Command' );
+
+?>
